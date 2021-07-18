9b1fba23c68a88bfd9b8e4c7bef45f021d7df2f0
jnsguillen <81977267+jnsguillen@users.noreply.github.com>
2021-07-19 01:35:39 +0800

    Add files via upload

---
+++ ELMS/BusinessLayer/Employees.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+	public class Employees
+	{
+		static public List<string> _names = new List<string>()
+		{"Guillen, Judea Nicole S.", "Navarro, Elton John S.", "Laluyan, Aliyah Mae Q."};
+
+		static public List<string> _positions = new List<string>()
+		{"Worker","Manager","CEO"};
+
+		static public List<string> _shifts = new List<string>()
+		{"Morning/AM","Morning/AM","Morning/AM" };
+	}
+}
+
---
+++ ELMS/BusinessLayer/Menu.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace BusinessLayer
+{
+    public class Menu
+    {
+        public static void selection() {
+
+			Console.WriteLine("----------------------------------------");
+			Console.WriteLine("EMPLOYEE LEAVES MANAGEMENT SYSTEM");
+			Console.WriteLine("----------------------------------------");
+			var CurrentDate = DateTime.Now;
+			Console.WriteLine(CurrentDate);
+			Console.WriteLine("Welcome User!");
+
+			Start:
+			int menu;
+			Console.WriteLine("Type the number of which you wish to access: ");
+			Console.WriteLine("----------------------------------------------");
+			Console.WriteLine("\n1: Request Form \n2: Pending Requests \n3: Record of Leaves");
+			menu = Convert.ToInt32(Console.ReadLine());
+
+			switch (menu)
+			{
+				case 1:
+					DataLayer.SqlData.InputRecord();
+					goto Start;
+					break;
+
+				case 2:
+					DataLayer.SqlData.ViewRequests();
+					goto Start;
+					break;
+
+				case 3:
+					DataLayer.SqlData.ShowRecords();
+					goto Start;
+					break;
+
+				default:
+					Console.WriteLine("No match found");
+					goto Start;
+					break;
+			}
+		}
+    }
+}
